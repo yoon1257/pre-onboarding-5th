90dcee842dae6ff4b152c9719269928812886a3b
yuna <yureve5772@gmail.com>
2022-10-16 23:28:58 +0900

    Add: 로그인 UI 구현

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,14 +5,8 @@ import { BsFillPersonFill } from "react-icons/bs";
 import { RiFileList3Line } from "react-icons/ri";
 import { AiTwotoneSetting } from "react-icons/ai";
 import { GiPowerButton } from "react-icons/gi";
-import {
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarFooter,
-  CDBSidebarHeader,
-  CDBSidebarMenu,
-  CDBSidebarMenuItem,
-} from "cdbreact";
+import { CDBSidebar, CDBSidebarFooter, CDBSidebarHeader } from "cdbreact";
+import { blue } from "../styles/theme";
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -25,33 +19,25 @@ const Sidebar = () => {
   const goMain = () => {
     navigate("/");
   };
+  const goSetting = () => {
+    navigate("/setting");
+  };
 
   return (
     <Wrapper>
-      <div
-        style={{ display: "flex", height: "100vh", overflow: "scroll initial" }}
-      >
-        <CDBSidebar textColor="#49cfd7" backgroundColor="#000">
+      <div className="align" style={{}}>
+        <CDBSidebar className="menu-bar">
           <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-            <a
-              href="/"
-              className="text-decoration-none"
-              style={{ color: "inherit" }}
-            >
+            <a href="/" className="text-decoration-none">
               Menu
             </a>
           </CDBSidebarHeader>
 
           <CDBSidebarFooter style={{ textAlign: "center" }}>
-            <div
-              className="sidebar-btn-wrapper"
-              style={{
-                padding: "20px 5px",
-              }}
-            >
+            <div className="sidebar-btn-wrapper">
               <BsFillPersonFill size="48" onClick={goUser} />
               <RiFileList3Line size="48" onClick={goList} />
-              <AiTwotoneSetting size="48" />
+              <AiTwotoneSetting size="48" onClick={goSetting} />
               <GiPowerButton size="48" onClick={goMain} />
             </div>
           </CDBSidebarFooter>
@@ -62,14 +48,25 @@ const Sidebar = () => {
 };
 const Wrapper = styled.div`
   float: left;
+  .align {
+    display: flex;
+    height: 100vh;
+    overflow: scroll initial;
+  }
+  .menu-bar {
+    color: ${blue};
+    background-image: url("/images/space.jpeg");
+  }
   .text-decoration-none {
     font-size: 28px;
+    color: inherit;
   }
   .sidebar-btn-wrapper {
     height: 580px;
     display: flex;
     justify-content: space-around;
     flex-direction: column;
+    padding: 20px 5px;
     align-items: center;
     cursor: pointer;
   }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,40 +1,42 @@
-import React, { useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { blue } from "../styles/theme";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
 import styled from "styled-components";
-const Main = () => {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
-  const [isValid, setIsValid] = useState(false);
 
-  const handleIdInput = (e) => {
-    const idValue = e.target.value;
-    setId(idValue);
-    idValue.length < 13 &&
-    idValue.length >= 4 &&
-    pw.length < 17 &&
-    pw.length >= 8
-      ? setIsValid(true)
-      : setIsValid(false);
-  };
-  const handlePwInput = (e) => {
-    const pwValue = e.target.value;
-    setPw(pwValue);
-    id.length < 13 &&
-    id.length >= 4 &&
-    pwValue.length < 17 &&
-    pwValue.length >= 1
-      ? setIsValid(true)
-      : setIsValid(false);
+const Main = () => {
+  const navigate = useNavigate();
+
+  const goUser = () => {
+    navigate("/user");
   };
   return (
     <>
       <MainStyle>
         <div className="outer-container">
           <div className="inner-container">
-            <h1>Login</h1>
             <div>
-              <input onChange={handleIdInput}></input>
-              <input onChange={handlePwInput}></input>
-              <button></button>
+              <h1>Login</h1>
+            </div>
+            <div className="align">
+              <InputGroup size="lg" className="mb-3">
+                <Form.Control
+                  placeholder="아이디를 입력해주세요"
+                  aria-describedby="inputGroup-sizing-sm"
+                />
+              </InputGroup>
+              <InputGroup size="lg" className="mb-3">
+                <Form.Control
+                  type="password"
+                  placeholder="비밀번호를 입력해주세요"
+                  aria-describedby="inputGroup-sizing-sm"
+                />
+              </InputGroup>
+              <Button size="lg" variant="outline-primary" onClick={goUser}>
+                login
+              </Button>
             </div>
           </div>
         </div>
@@ -43,23 +45,50 @@ const Main = () => {
   );
 };
 const MainStyle = styled.div`
-  /* background-image: url("/images/space.jpeg"); */
-  background-color: #000;
+  background-image: url("/images/space.jpeg");
   background-size: cover;
-  background-repeat: no-repeat;
   height: 100vh;
   .outer-container {
-    border: 1px solid blue;
+    border-left: 1px solid ${blue};
     height: 100%;
     display: flex;
     align-items: center;
     .inner-container {
       display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
       margin: 0 auto;
-      border: 1px solid skyblue;
+      border: 1px solid ${blue};
       border-radius: 10px;
       width: 80%;
       height: 500px;
+      h1 {
+        text-align: center;
+        margin-bottom: 100px;
+        color: ${blue};
+      }
+      .align {
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        .input-group-text {
+          color: ${blue};
+          background-color: transparent;
+          border: 1px solid ${blue};
+        }
+        .form-control {
+          background-color: transparent;
+          color: #fff;
+          border: 1px solid ${blue};
+        }
+        .btn-outline-primary {
+          --bs-btn-hover-bg: ${blue};
+          --bs-btn-border-color: ${blue};
+          --bs-btn-color: ${blue};
+          --bs-btn-hover-border-color: ${blue};
+        }
+      }
     }
   }
 `;
